9c05013cc69eca7fa9495ee2f76f0403e64b78aa
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-11-06 22:50:57 -0500

    Solved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    };
+
+    if (typeof(num1) !== "number" || typeof(num2) !== "number") {
+        return "ERROR";
+    };
+    
+    let result = 0;
+    
+    let mayor, menor;
+    
+    if (num1 > num2) {
+        mayor = num1
+        menor = num2
+    } else {
+        mayor = num2
+        menor = num1
+    };
+
+    for (let i = menor; i <= mayor; i++) {
+        result = result + i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
