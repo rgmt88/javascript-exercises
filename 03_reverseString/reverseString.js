c64bf18867f610817738173d862296694e45479a
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-11-06 21:16:14 -0500

    Solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let result = "";
+    let length = string.length
 
+    for (let i = 0; i < length; i++) {
+        result = result + string.charAt(length - 1 - i)
+    };
+
+    return result;
 };
 
 // Do not edit below this line
