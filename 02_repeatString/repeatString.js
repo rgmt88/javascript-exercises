0592e798498c9d8e24770c99dc3d255fadd68017
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-11-06 21:01:28 -0500

    Solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    result = "";
+    for (let i = 0; i < num; i++) {
+        result = result + string;
+    };
+    
+    return result;
 };
 
 // Do not edit below this line
