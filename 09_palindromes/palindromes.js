41872afcaf2f05c37a09f57e2b6ef417b50e027a
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-16 15:14:54 -0500

    Solved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    if (typeof str === 'string') {
+        const regex = /[\W_]/g;
+        cleanedStr = str.replace(regex, '').toLowerCase();
+        reverseStr = cleanedStr.split('').reverse().join('');
+        if (cleanedStr === reverseStr) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        throw new TypeError('Input must be a string');
+    }
 };
 
 // Do not edit below this line
