eb75b9607042fb7096782039082bf4e812c0e1a1
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-16 15:45:41 -0500

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    index = +index;
+    let fibo = [1, 1];
+    
+    if(index <= 0) {
+        return 'OOPS';
+    };
 
+    if(index === 1 || index === 2) {
+        return 1;
+    } else {
+        for(let i = 2; i < index; i++) {
+            fibo.push(fibo[i - 1] + fibo[i - 2]);
+        }
+    }
+    return fibo[index - 1];
 };
 
 // Do not edit below this line
