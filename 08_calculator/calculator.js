8e7c53bb5947840a9ff219e7f05e041f752914a6
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-15 21:32:58 -0500

    solved

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  array.map(number => sum += number);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let total = 1;
+  array.forEach(number => total *= number);
+  return total;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;
 };
 
 // Do not edit below this line
