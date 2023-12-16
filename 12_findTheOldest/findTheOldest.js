b6f57b6d764240265759e1e6e8863748c3286db3
rgmt88 <65685133+rgmt88@users.noreply.github.com>
2023-12-16 16:45:45 -0500

    Solved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(peopleArray) {
+    const currentYear = new Date().getFullYear();
 
+    peopleArray.sort((a, b) => {
+        const ageA = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const ageB = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        return ageB - ageA;
+    });
+
+    return peopleArray[0];
 };
 
 // Do not edit below this line
